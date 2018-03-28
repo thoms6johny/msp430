c29dce6ab43b6d92f6dbad56d3f0c6369da8a1b7
thoms6johny <thoms.johny@gmail.com>
2018-03-28 22:24:30 +0530

    Add files via upload

---
+++ ldr_sen.ino
@@ -0,0 +1,68 @@
+int sensorPin = A0; 
+int sensorValue = 0;
+#define MAX_ADC_READING           1023
+#define ADC_REF_VOLTAGE           3.3
+#define REF_RESISTANCE            2900  // measure this for best results and put the value according to what is connected to the ldr in series
+#define LUX_CALC_SCALAR           1251893100
+#define LUX_CALC_EXPONENT         -1.4059
+
+const int numReadings = 10;
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+
+
+void setup() {
+ Serial.begin(9600);
+   // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+
+  }
+}
+
+
+void loop(){
+  int ldrRawData;
+  float resistorVoltage, ldrVoltage;
+  float ldrResistance;
+  float ldrLux;
+  Serial.print("Lux1:");
+  Serial.println(fun_ldr(sensorPin));
+  }
+
+float fun_ldr(int pin)
+{
+  int readIndex = 0;
+  int ldrRawData;
+  float resistorVoltage, ldrVoltage;
+  float ldrResistance;
+  float ldrLux;
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(pin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  //Serial.println(average);
+  delay(1); 
+  ldrRawData = average;
+  resistorVoltage = ((float)ldrRawData / MAX_ADC_READING) * ADC_REF_VOLTAGE;
+  ldrVoltage = ADC_REF_VOLTAGE - resistorVoltage;
+  ldrResistance = ldrVoltage/resistorVoltage * REF_RESISTANCE;
+  ldrLux = LUX_CALC_SCALAR * powf(ldrResistance, LUX_CALC_EXPONENT); 
+  return (ldrLux); //prints the values coming from the sensor on the screen   
+  }
---
+++ lm35.ino
@@ -0,0 +1,24 @@
+void setup()
+{
+  Serial.begin(9600);   // opens serial port, sets data rate to 9600 bps  
+}
+
+void loop()
+{
+  Serial.print("Temps:");
+  Serial.print(temp_sen(A0));
+  Serial.println(" C");
+  delay(500);
+ 
+}
+int temp_sen (int pin)
+{
+  int value=0;            //initializing variables
+  float volts=0.0;      
+  float temp=0.0;
+  value=analogRead(pin);
+  volts=(value/1024.0)*3.3;      //conversion to volts
+  temp= volts*100.0;             //conversion to temp Celsius
+  return temp;
+  
+ }  
